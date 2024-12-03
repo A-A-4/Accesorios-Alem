a77fccf927316f4991f25313fd1722333079e37a
A-A-4 <juagus1111@gmail.com>
2024-12-03 04:00:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ document.addEventListener('click', (e) => {
         dropdown.classList.remove('active'); // Cierra el menú si se hace clic fuera de él
     }
 });
+
+
+
+
+
+
+// Seleccionamos todas las imágenes con la clase 'zoomable'
+const images = document.querySelectorAll('.zoomable');
+
+images.forEach(image => {
+  image.addEventListener('click', function() {
+    // Añadimos la clase 'zoomed' a la imagen cuando se hace clic
+    image.classList.toggle('zoomed');
+    
+    // Al hacer clic, añadimos un fondo oscuro (cambiamos el 'body')
+    document.body.classList.toggle('dark-mode');
+
+    if (this.classList.contains('zoomed')) {
+        document.body.classList.add('productos-active'); // Activar fondo oscuro en todas las cuadriculas
+    } else {
+        document.body.classList.remove('productos-active'); // Desactivar fondo oscuro
+    }
+  });
+
+});
+
+
+
+
+
+
+
+
